f09281e36b9aac9558a5fca46be51b819014ae7b
ty-v1 <y-tomida@ist.osaka-u.ac.jp>
2019-09-12 18:57:14 +0900

    提出ページのパーサーを追加

---
+++ src/parser.js
@@ -0,0 +1,55 @@
+const cheerio = require('cheerio');
+const _ = require('lodash');
+
+/**
+ * 提出一覧ページをパースしてIDを取得する
+ * */
+exports.parseSubmissionListPage = function (htmlDocument) {
+    const $ = cheerio.load(htmlDocument);
+
+    // 全ての行を取得する
+    const trs = $('table tbody').find('tr');
+
+    // リンク一覧を取得する
+    const links = [];
+    trs.each((i, tr) => {
+        const tds = tr.children.filter(e => e.name === 'td');
+        const tailTd = _.last(tds);
+
+        if (_.isNil(tailTd)) {
+            return;
+        }
+
+        const a = tailTd.children.find(e => e.name === 'a');
+        if (_.isNil(a)) {
+            return;
+        }
+        links.push(a.attribs['href'] || '');
+    });
+
+    // 空文字を削除してから返す
+    return links.filter(e => !_.isEmpty(e));
+};
+
+/**
+ * 提出ページを解析してソースコードを取得する
+ * */
+exports.parseSubmissionPage = function (htmlDocument) {
+    const $ = cheerio.load(htmlDocument);
+    const pres = $('#submission-code');
+    const sourceCodes = [];
+
+    pres.each((i, pre) => {
+        const innerText = _.first(pre.children);
+        if (_.isNil(innerText)) {
+            return;
+        }
+
+        const sourceCode = innerText.data || '';
+        sourceCodes.push(sourceCode);
+    });
+
+    // ゴミを消してから返す
+    const sourceCode = _.first(sourceCodes);
+    return sourceCode || '';
+};
