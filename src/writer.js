eb29deb90a09e205fc501b009d7d3cde82b7dd8e
ty-v1 <y-tomida@ist.osaka-u.ac.jp>
2019-09-12 19:10:38 +0900

    IO処理を実装

---
+++ src/writer.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * ソースコードを書き込む
+ * */
+exports.writeSourceCode = function (outDir, contestId, submissionId, langague, sourceCode) {
+    const sourceCodeDir = path.join(outDir, contestId, langague);
+
+    // ディレクトリがないときは同期的にディレクトリを作る
+    if (!fs.existsSync(sourceCodeDir)) {
+        fs.mkdir(sourceCodeDir);
+    }
+
+    const sourceCodePath = path.join(sourceCodeDir, `${submissionId}.java`);
+    const writer = fs.createWriteStream(sourceCodePath, 'utf8');
+    write.on('finish', () => {
+        console.log(`Write source code to ${sourceCodePath}`);
+    });
+    write.on('error', function () {
+        console.log(`Cannot write file ${sourceCodePath}`);
+    });
+    writer.write(sourceCode);
+};
