d3a861eabec4d24e6cf97aa97793e4b22614dce2
ty-v1 <y-tomida@ist.osaka-u.ac.jp>
2019-09-12 18:56:56 +0900

    提出ページを取得する関数を実装

---
+++ src/fetcher.js
@@ -0,0 +1,35 @@
+const request = require('request-promise');
+
+function formatter(value) {
+    return ('000' + value).slice(-3);
+}
+
+/**
+ * 提出一覧ページを取得する
+ * */
+exports.fetchSubmissionListPage = async function (contestId, taskId, languageId, page) {
+    const contestName = `abc${formatter(contestId)}`;
+    const taskName = `${contestName}_${taskId}`;
+    const option = {
+        url: `https://atcoder.jp/contests/${contestName}/submissions`,
+        qs: {
+            'f.Task': taskName,
+            'f.Language': languageId,
+            'f.Status': 'AC',
+            page
+        }
+    };
+
+    return await request(option);
+};
+
+/**
+ * 提出ページを取得する
+ * */
+exports.fetchSubmissionPage = async function (path) {
+    const option = {
+        url: `https://atcoder.jp${path}`
+    };
+
+    return await request(option);
+};
