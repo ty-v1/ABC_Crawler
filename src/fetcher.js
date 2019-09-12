3b4dd62c9a9f139f83daa4946ca39d9432406c63
ty-v1 <y-tomida@ist.osaka-u.ac.jp>
2019-09-12 20:01:55 +0900

    スリープ処理を追加

--- src/fetcher.js
+++ src/fetcher.js
@@ -4,6 +4,12 @@ function formatter(value) {
     return ('000' + value).slice(-3);
 }
 
+function sleep(time) {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(), time);
+    });
+}
+
 const languages = {
     'java7': 3015,
     'java8': 3016,
@@ -13,6 +19,8 @@ const languages = {
  * 提出一覧ページを取得する
  * */
 exports.fetchSubmissionListPage = async function (contestId, taskId, language, page) {
+    await sleep(2000);
+
     const contestName = `abc${formatter(contestId)}`;
     const taskName = `${contestName}_${taskId}`;
     const option = {
@@ -24,7 +32,6 @@ exports.fetchSubmissionListPage = async function (contestId, taskId, language, p
             page
         }
     };
-
     return await request(option);
 };
 
@@ -32,9 +39,15 @@ exports.fetchSubmissionListPage = async function (contestId, taskId, language, p
  * 提出ページを取得する
  * */
 exports.fetchSubmissionPage = async function (path) {
+    await sleep(2000);
+
     const option = {
         url: `https://atcoder.jp${path}`
     };
-
-    return await request(option);
+    const text = await request(option);
+    const id = (/\/\d+/.exec(path))[0].replace('/', '');
+    return {
+        text,
+        id
+    };
 };
