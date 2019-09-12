be14da21dc3f2ec9e2bcbf984c777232e75e82d6
ty-v1 <y-tomida@ist.osaka-u.ac.jp>
2019-09-12 22:49:05 +0900

    コンフィグを出力する処理を実装

--- index.js
+++ index.js
@@ -17,7 +17,21 @@ async function run(options) {
     }
 }
 
+function logConfig(options) {
+    console.log('=========================\n');
+
+    console.log(`All: ${options['all']}`);
+    console.log(`OutDir: ${options['out']}`);
+    if (options['all']) {
+        console.log(`Contest: ${options['contest']}`);
+    }
+    console.log(`Langs: ${options['langs']}`);
+
+    console.log('=========================');
+}
+
 //
 // 引数をパースする
 const options = commandLineArgs(definitions);
+logConfig(options);
 run(options).then(() => console.log('finish'));
